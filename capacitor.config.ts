baaecc03220d1ab4aee21b9d293fe866eca7871b
Javier León Soler <jaleso@alumni.uv.es>
2024-10-17 08:34:10 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BioApp',
+  webDir: 'www'
+};
+
+export default config;
