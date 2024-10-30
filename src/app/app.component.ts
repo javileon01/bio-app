48b897cd1facabb62577bd772f7b38ed285dac5a
jaleso <jaleso@alumni.uv.es>
2024-10-30 13:47:43 +0100

    Corrección de títulos y mejoras en el diseño del menú y páginas de contacto e información; se añade funcionalidad para ocultar el header en ciertas páginas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  showHeader = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Ocultar el header en la página de 'informacio' o cualquier otra página
+        this.showHeader = !['/informacio', '/contacte', '/webs-recomanats', '/manual-usuari'].includes(event.url);
+      }
+    });
+  }
 }
--- src/pages/contacte/contacte.page.ts
+++ src/pages/contacte/contacte.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contacte',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactePage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController) { }
 
   ngOnInit() {
+    // Puedes inicializar datos aquí si es necesario
   }
 
+  goBack() {
+    this.navController.back();
+  }
 }
