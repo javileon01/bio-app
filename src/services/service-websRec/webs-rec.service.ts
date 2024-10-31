7fcb9e32f87d3479e3238402c9644cac74109c70
jaleso <jaleso@alumni.uv.es>
2024-10-31 14:24:19 +0100

    Mejoras en el estilo de algunas páginas. Implementación del servicio para obtener datos de webs recomendadas.

--- src/services/service-websRec/webs-rec.service.ts
+++ src/services/service-websRec/webs-rec.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsRecService {
+  private apiUrl = 'http://bioaps.uv.es/totesw/'; // URL de la API
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Método para obtener los datos de las webs recomendadas
+  obtenerDatosTexto(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
