67bed491a892a91451b44c757aabc61d439893d7
jaleso <jaleso@alumni.uv.es>
2024-11-07 21:29:30 +0100

    Mejoras en el estilizado para la representación para móviles, combinación de estilos entre el main header y las tabs arreglada y posiblemente definitiva.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,9 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        // Ocultar el header en la página de 'informacio' o cualquier otra página
-        const currentUrl = event.url.split(';')[0]; // Cambiado a ';' para manejar el formato de la URL
-        console.log('Navegando a:', event.url); // Verifica la URL
-        this.showHeader = !['/informacio', '/contacte', '/webs-recomanats', '/manual-usuari','/glossari-def'].includes(currentUrl);
+        // Aquí puedes ocultar el encabezado en rutas específicas si lo necesitas
+        const currentUrl = event.url.split(';')[0];
+        this.showHeader = !['/informacio', '/contacte', '/webs-recomanats', '/manual-usuari', '/glossari-def'].includes(currentUrl);
       }
     });
   }
--- src/pages/tabs/tabs.page.ts
+++ src/pages/tabs/tabs.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage implements OnInit {
+  showHeader = true;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentUrl = event.url.split(';')[0];
+        // Esconde el header en rutas específicas
+        this.showHeader = !['/informacio', '/contacte', '/webs-recomanats', '/manual-usuari', '/glossari-def'].includes(currentUrl);
+      }
+    });
+  }
 }
