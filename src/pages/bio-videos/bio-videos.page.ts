292b93db834c1272c404b7a76c88114e2f066df4
jaleso <jaleso@alumni.uv.es>
2024-11-12 20:30:29 +0100

    Renombrada función de filtrado de videos y mejorada la carga de datos con manejo de suscripción más claro. Añadido método para formatear títulos de videos.

--- src/pages/bio-videos/bio-videos.page.ts
+++ src/pages/bio-videos/bio-videos.page.ts
@@ -24,31 +24,31 @@ export class BioVideosPage implements OnInit {
 
   // Cargar todos los videos desde el servicio
   cargarVideos() {
-    this.videosService.obtenerVideos().subscribe(
-      (data) => {
+    this.videosService.obtenerVideos().subscribe({
+      next: (data) => {
         this.videos = data;
         this.filteredVideos = data; // Inicialmente, mostrar todos los videos
       },
-      (error) => {
+      error: (error) => {
         console.error('Error al cargar los videos:', error);
       }
-    );
+    });
   }
 
   // Cargar todos los textos introductorios desde el servicio
   cargarTextosIntroductorios() {
-    this.contenidoService.obtenerDatosTexto().subscribe(
-      (data) => {
+    this.contenidoService.obtenerDatosTexto().subscribe({
+      next: (data) => {
         this.textosIntroductorios = data; // Cargar todos los textos sin filtrar
       },
-      (error) => {
+      error: (error) => {
         console.error('Error al cargar los textos introductorios:', error);
       }
-    );
+    });
   }
 
   // Filtrar los videos en tiempo real a medida que el usuario escribe en la barra de búsqueda
-  filterVideos(event: any) {
+  filtrarVideos(event: any) {
     const searchTerm = event.target.value.toLowerCase();
     if (!searchTerm || searchTerm.trim() === '') {
       this.filteredVideos = this.videos;
@@ -57,6 +57,11 @@ export class BioVideosPage implements OnInit {
         video.titol.toLowerCase().includes(searchTerm)
       );
     }
+  }
+
+  // Método para obtener el texto introductorio de un video formateado
+  obtenerTituloFormateado(titulo: string) {
+    return titulo.endsWith('(BioApS)') ? titulo.slice(0, -9) : titulo;
   }
 
   // Abrir el video en una página externa
