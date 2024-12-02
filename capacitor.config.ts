07b384d7e0eb657b2ae70896788cc16087b2afe7
jaleso <jaleso@alumni.uv.es>
2024-12-02 15:06:50 +0100

    Añadida nueva lógica de configuración para el splash screen y se han agregado nuevos íconos para iOS y Android. Actualizada la dependencia 'cordova-res' en package.json.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'BioApp',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Tiempo que el splash se muestra (en milisegundos)
+      launchAutoHide: true, // Oculta automáticamente el splash después del tiempo configurado
+      backgroundColor: '#ffffff', // Color de fondo del splash screen
+      androidScaleType: 'CENTER_CROP', // Escalado para Android
+      showSpinner: true, // Muestra un spinner de carga
+      spinnerColor: "#000000", // Color del spinner
+    }
+  }
 };
 
 export default config;
